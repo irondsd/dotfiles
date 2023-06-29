d1d1c76a009879d32abc3dd73b9e2196067ea0de
Konstantin Mednikov <irondsd@gmail.com>
2023-06-29 18:32:22 -0300

    add brew install

---
+++ brew/install.sh
@@ -0,0 +1,17 @@
+brew install git
+brew install bat
+brew install folderify
+brew install pass
+brew install pass-otp
+brew install pngpaste
+brew install gnupg
+brew install imagemagick
+brew install starship
+brew install webp
+brew install neovim
+brew install zbar
+brew install bat
+
+brew install --cask cron
+brew install --cask firefox
+brew install --cask cleanmymac
