99b7f7e31470ff5718372a30cda8a95bbe410b28
Konstantin Mednikov <irondsd@gmail.com>
2023-06-29 20:52:24 -0300

    macos set defaults

---
+++ install/macos/defaults.sh
@@ -0,0 +1,14 @@
+# Set dock hide and unhide instantly
+defaults write com.apple.dock autohide-delay -float 0
+defaults delete com.apple.dock autohide-delay
+defaults write com.apple.dock autohide-time-modifier -int 0
+killall Dock
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Define where to save screenshots
+defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots"
