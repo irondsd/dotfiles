a134dc80fa546e721ae70c4da792273a3a5bbd85
Konstantin Mednikov <irondsd@gmail.com>
2023-06-29 20:52:04 -0300

    add brew install

--- brew/install.sh
+++ brew/install.sh
@@ -11,7 +11,17 @@ brew install webp
 brew install neovim
 brew install zbar
 brew install bat
+brew install volta
+brew install ripgrep
 
 brew install --cask cron
 brew install --cask firefox
 brew install --cask cleanmymac
+brew install --cask sublime-text
+brew install --cask postman
+brew install --cask raycast
+brew install --cask the-unarchiver
+brew install --cask tunnelblick
+brew install --cask visual-studio-code
+brew install --cask transmission
+brew install --cask notion
