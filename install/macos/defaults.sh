177aabad4f7fa04e9ef4668396e7e8e72e94014f
Konstantin Mednikov <irondsd@gmail.com>
2023-06-29 20:58:53 -0300

    improve defaults

--- install/macos/defaults.sh
+++ install/macos/defaults.sh
@@ -4,6 +4,7 @@
 defaults write com.apple.dock autohide-delay -float 0
 defaults delete com.apple.dock autohide-delay
 defaults write com.apple.dock autohide-time-modifier -int 0
+defaults write com.apple.dock expose-animation-duration -float 0
 killall Dock
 
 # Set a really fast key repeat.
@@ -14,3 +15,12 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
 # Define where to save screenshots
 defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots"
+
+# Show all filename extensions.
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable terminal prompt
+touch ~/.hushlogin
