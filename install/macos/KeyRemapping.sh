1ffa8b8f9879d555fc3b711e8f1aa7563ea10b1a
Konstantin Mednikov <irondsd@gmail.com>
2025-07-21 08:18:44 -0300

    change keymapping

---
+++ install/macos/KeyRemapping.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0xC00000221,"HIDKeyboardModifierMappingDst":0x700000068},{"HIDKeyboardModifierMappingSrc":0xC000000CF,"HIDKeyboardModifierMappingDst":0x70000006E},{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000006D}]}'
+exit 0
